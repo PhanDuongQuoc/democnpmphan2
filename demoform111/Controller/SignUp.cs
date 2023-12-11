8c852482ad12e36d9cff4577206e1bf9de391058
Phan Duong Quoc <phanduongquoc111@gmail.com>
2023-12-11 14:23:56 +0700

    create SignUp

---
+++ demoform111/Controller/SignUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoform111.Controller
+{
+    internal class SignUp
+    {
+    }
+}
